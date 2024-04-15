e1bd9937f288c1fa5350955dfd4e130b97b84741
Gen <54583542+gen740@users.noreply.github.com>
2024-04-16 00:47:00 +0900

    [Delete] hooks

--- src/hooks/useTaskLists.ts
+++
@@ -1,8 +0,0 @@
-import { useCachedPromise } from "@raycast/utils";
-import type { TaskLists } from "../type";
-import getTaskLists from "../utils/getTaskLists";
-
-export function useTaskLists() {
-  const { data, revalidate } = useCachedPromise(getTaskLists, []);
-  return { data: data as TaskLists, revalidate };
-}
