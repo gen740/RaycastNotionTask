b12c10d679e4b65d0ba475b5ac0c6f283df6a237
Gen <54583542+gen740@users.noreply.github.com>
2024-04-16 00:47:44 +0900

    [Update] search-tasks

--- src/search-tasks.tsx
+++ src/search-tasks.tsx
@@ -1,116 +1,124 @@
 import { Action, ActionPanel, Color, Detail, List } from "@raycast/api";
-import { useTaskLists } from "./hooks/useTaskLists";
-import type { Task } from "./type";
-import { withNotionAccessToken } from "./utils/accessToken";
+import type { Task, TaskLists } from "./type";
 import { changeTaskStatus } from "./utils/changeTaskStatus";
+import getTaskLists from "./utils/getTaskLists";
+import { useCachedPromise } from "@raycast/utils";
 import { useEffect, useState } from "react";
 
-function taskElement(task: Task, revalidate: () => void) {
-  const [detailMarkdown, setDetailMarkdown] = useState<string | undefined>();
-  let icon = {
-    source: "icon/kanban_status_backlog.png",
-    tintColor: Color.Red,
+const TaskMarkdown = ({
+  icon,
+  task,
+}: {
+  icon: {
+    source: string;
+    tintColor: Color;
   };
-  switch (task.status) {
-    case "done":
-      icon = {
-        source: "icon/kanban_status_completed.png",
-        tintColor: Color.Blue,
-      };
-      break;
-    case "in progress":
-      icon = {
-        source: "icon/kanban_status_started.png",
-        tintColor: Color.Green,
-      };
-      break;
-    case "not started":
-      icon = {
-        source: "icon/kanban_status_not_started.png",
-        tintColor: Color.Orange,
-      };
-      break;
-  }
+  task: Task;
+}) => {
+  const [md, setMd] = useState<string | undefined>(undefined);
 
   useEffect(() => {
-    task.contentMarkdown?.().then((value) => {
-      setDetailMarkdown(value ?? "");
-    });
-  }, [task]);
-
+    task.contentMarkdown?.().then(setMd);
+  });
   return (
-    <List.Item
-      icon={icon}
-      title={task.title}
-      subtitle={task.dueDate?.toDateString() ?? ""}
-      key={task.title}
-      actions={
-        <ActionPanel title="Notion Task">
-          <Action.Push
-            title={detailMarkdown ?? ""}
-            target={((detailMarkdown) => {
-              return (
-                <Detail
-                  isLoading={detailMarkdown === undefined}
-                  markdown={detailMarkdown}
-                  metadata={
-                    <Detail.Metadata>
-                      <Detail.Metadata.Label
-                        icon={icon}
-                        title="Title"
-                        text={task.title}
-                      />
-                      <Detail.Metadata.Label
-                        title="期限"
-                        text={task.dueDate?.toDateString() ?? "No date"}
-                      />
-                    </Detail.Metadata>
-                  }
-                />
-              );
-            })(detailMarkdown)}
+    <Detail
+      isLoading={md === undefined}
+      markdown={`${md}`}
+      metadata={
+        <Detail.Metadata>
+          <Detail.Metadata.Label icon={icon} title="Title" text={task.title} />
+          <Detail.Metadata.Label
+            title="期限"
+            text={task.dueDate?.toDateString() ?? "No date"}
           />
-          <Action.OpenInBrowser
-            url={`https://notion.so/${task.pageId.replace(/-/g, "")}`}
-            shortcut={{ modifiers: ["cmd"], key: "o" }}
-          />
-          <Action
-            title="Mark As Done"
-            onAction={() => {
-              changeTaskStatus(task.pageId, "done");
-              setTimeout(revalidate, 200);
-            }}
-            shortcut={{ modifiers: ["cmd"], key: "d" }}
-          />
-          <Action
-            title="Mark As In Progress"
-            onAction={() => {
-              changeTaskStatus(task.pageId, "in progress");
-              setTimeout(revalidate, 200);
-            }}
-            shortcut={{ modifiers: ["cmd"], key: "i" }}
-          />
-          <Action
-            title="Mark As Not Started"
-            onAction={() => {
-              changeTaskStatus(task.pageId, "not started");
-              setTimeout(revalidate, 200);
-            }}
-            shortcut={{ modifiers: ["cmd"], key: "n" }}
-          />
-          <Action
-            title="Reload"
-            onAction={revalidate}
-            shortcut={{ modifiers: ["cmd"], key: "r" }}
-          />
-        </ActionPanel>
+        </Detail.Metadata>
       }
     />
   );
-}
+};
+
+const Items = ({
+  items,
+  revalidate,
+}: { items: TaskLists | undefined; revalidate: () => void }) => {
+  return items?.map((task) => {
+    let icon = {
+      source: "icon/kanban_status_backlog.png",
+      tintColor: Color.Red,
+    };
+    switch (task.status) {
+      case "done":
+        icon = {
+          source: "icon/kanban_status_completed.png",
+          tintColor: Color.Blue,
+        };
+        break;
+      case "in progress":
+        icon = {
+          source: "icon/kanban_status_started.png",
+          tintColor: Color.Green,
+        };
+        break;
+      case "not started":
+        icon = {
+          source: "icon/kanban_status_not_started.png",
+          tintColor: Color.Orange,
+        };
+        break;
+    }
+    return (
+      <List.Item
+        title={`${task.title}`}
+        key={task.title}
+        icon={icon}
+        actions={
+          <ActionPanel>
+            <Action.Push
+              title="push"
+              target={<TaskMarkdown icon={icon} task={task} />}
+            />
+            <Action.OpenInBrowser
+              url={`https://notion.so/${task.pageId.replace(/-/g, "")}`}
+              shortcut={{ modifiers: ["cmd"], key: "o" }}
+            />
+            <Action
+              title="Mark As Done"
+              onAction={() => {
+                changeTaskStatus(task.pageId, "done");
+                setTimeout(revalidate, 200);
+              }}
+              shortcut={{ modifiers: ["cmd"], key: "d" }}
+            />
+            <Action
+              title="Mark As In Progress"
+              onAction={() => {
+                changeTaskStatus(task.pageId, "in progress");
+                setTimeout(revalidate, 200);
+              }}
+              shortcut={{ modifiers: ["cmd"], key: "i" }}
+            />
+            <Action
+              title="Mark As Not Started"
+              onAction={() => {
+                changeTaskStatus(task.pageId, "not started");
+                setTimeout(revalidate, 200);
+              }}
+              shortcut={{ modifiers: ["cmd"], key: "n" }}
+            />
+            <Action
+              title="Reload"
+              onAction={revalidate}
+              shortcut={{ modifiers: ["cmd"], key: "r" }}
+            />
+          </ActionPanel>
+        }
+      />
+    );
+  });
+};
 
 export function Search() {
-  const { data, revalidate } = useTaskLists();
+  const { data, revalidate } = useCachedPromise(getTaskLists, []);
 
   const today = new Date();
   const oneWeekAgo = new Date(today.setDate(today.getDate() - 3));
@@ -135,13 +143,14 @@ export function Search() {
         }
         return 4;
       };
-
       return statusToNum(a.status) - statusToNum(b.status);
     });
 
   return (
-    <List>{filteredTask?.map((task) => taskElement(task, revalidate))}</List>
+    <List>
+      <Items items={filteredTask} revalidate={revalidate} />
+    </List>
   );
 }
 
-export default withNotionAccessToken(Search);
+export default Search;
